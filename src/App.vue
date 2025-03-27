332285c64b83679ad6c65a67196473d11e054816
Richard Bowman <richard.bowman@cantab.net>
2025-03-27 14:05:25 +0000

    Initial skeleton with BinCalendar

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { ref } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import BinCalendar from './components/BinCalendar.vue'
+
+const greenBinFirstDay = ref(Date(2025, 3, 13))
+</script>
+
+<template>
+  <header></header>
+
+  <main>
+    <div class="wrapper">
+      <h3>This is the bin wrapper</h3>
+      <BinCalendar :green-bin-first-day />
+    </div>
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/BinCalendar.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+
+import Calendar from 'js-year-calendar'
+import 'js-year-calendar/dist/js-year-calendar.css'
+
+defineProps({
+  greenBinFirstDay: {
+    type: String,
+    required: true,
+  },
+  /*grayBinFirstDay: {
+    type: String,
+    required: true,
+  },
+  recyclingBinFirstDay: {
+    type: String,
+    required: true,
+  },
+  foodBinFirstDay: {
+    type: String,
+    required: true,
+  }*/
+})
+
+const calendar = ref(null)
+
+onMounted(() => {
+  console.log('cal', calendar)
+  new Calendar(calendar.value)
+})
+</script>
+
+<template>
+  <div class="calendarContainer">
+    <h2>Calendar</h2>
+    <div ref="calendar"></div>
+  </div>
+</template>
+
+<style scoped></style>
